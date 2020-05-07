c47b1091dda1fd06447c5feef824d88033987632
Natalya-dev <pashkevich_natalya111@mail.ru>
2020-05-07 18:21:52 +0300

    upload project

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,12 +1,11 @@
-package pages;
-
-public class BasePage {
 // IntelliJ API Decompiler stub source generated from a class file
-    // Implementation of methods is not available
+// Implementation of methods is not available
 
 package pages;
 
-    public class BasePage extends net.serenitybdd.core.pages.PageObject {
-        protected BasePage(org.openqa.selenium.WebDriver driver) { /* compiled code */ }
+public class BasePage extends net.serenitybdd.core.pages.PageObject {
+    protected BasePage(org.openqa.selenium.WebDriver driver) { /* compiled code */ }
+
+    public BasePage() {
     }
 }
