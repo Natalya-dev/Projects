ea7c477326d4430b17160b999717365c2ca6e16d
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-09 16:54:30 +0300

    Update MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,16 +1,51 @@
-// IntelliJ API Decompiler stub source generated from a class file
-// Implementation of methods is not available
-
 package pages;
 
-@net.thucydides.core.annotations.DefaultUrl("http://automationpractice.com/index.php")
-public class MainPage extends pages.BasePage {
-    @org.openqa.selenium.support.FindBy(css = "a.login")
-    private org.openqa.selenium.WebElement signInButton;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-    public MainPage(org.openqa.selenium.WebDriver driver) { /* compiled code */ }
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public void openMainPage() { /* compiled code */ }
+@DefaultUrl("http://automationpractice.com/index.php")
+public class MainPage<Webelement> extends BasePage {
+    @FindBy(css = "a.login")
 
-    public void clickOnSignInButton() { /* compiled code */ }
+    private WebElement signInButton;
+
+    @FindBy (xpath = ("//*[@id='create-account_form']/div/p"))
+            private WebElement createAnAccountText;
+    @FindBy (id = "email")
+            private Webelement emailAddressField;
+    @FindBy (id = "passwd")
+            private WebElement passwordField;
+
+    public MainPage(WebDriver driver) { super(driver); }
+
+    public void openMainPage() { open(); }
+
+
+    public void clickOnSignInButton() {element(signInButton).click();
+
+        public void checkText() {
+            String text = element(signInButton).getText();
+            Pattern pattern = Pattern.compile("(^Please\\senter\\s)");
+            Matcher matcher = pattern.matcher(text);
+
+            if(matcher.find()) {
+                System.out.println("Нашел");
+            } else {
+                System.out.println("Нe нашел");
+            }
+
+        }
+        public void fillInEmailAddressField (String email) {
+            element (emailAddressField).sendKeys(email);
+        }
+
+        public void fillInPasswordField (String password) {
+            element (passwordField).sendKeys(password);
+        }
+    }
 }
