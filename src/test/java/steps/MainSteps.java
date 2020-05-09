8d088e4b75ba863b87e0bf032566eb68cddef41d
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-09 17:02:50 +0300

    Update MainSteps.java

--- src/test/java/steps/MainSteps.java
+++ src/test/java/steps/MainSteps.java
@@ -1,16 +1,40 @@
-// IntelliJ API Decompiler stub source generated from a class file
-// Implementation of methods is not available
-
 package steps;
 
-public class MainSteps extends net.thucydides.core.steps.ScenarioSteps {
-    private pages.MainPage onPage;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+import pages.MainPage;
 
-    public MainSteps() { /* compiled code */ }
+public class MainSteps extends ScenarioSteps {
+    private MainPage onPage;
 
-    @net.thucydides.core.annotations.Step
-    public void clickOnSignInButton() { /* compiled code */ }
+    public MainSteps() {
+    }
 
-    @net.thucydides.core.annotations.Step
-    public void openMainPage() { /* compiled code */ }
-}
+    @Step
+    public void clickOnSignInButton() {
+        this.onPage.clickOnSignInButton();
+    }
+
+    @Step
+    public void openMainPage() { this.onPage.openMainPage(); }
+
+    @Step
+    public void checkText() {
+        onPage.checkText () ;
+        @Step
+        public void fillInEmailAddressField (String email) {
+            onPage.fillInEmailAddressField (email);
+
+            @Step
+                    public void fillInPasswordField (String password) {
+                onPage.fillInPasswordField ();
+
+            }
+        }
+    }
+
+    public void fillInEmailAddressField(String email) {
+    }
+
+    public void fillInPasswordField(String password) {
+    }
