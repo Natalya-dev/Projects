0850c41293e4b86e49cf22c3b5be258b5ee446ca
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-09 17:02:08 +0300

    Update BasePage.java

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,12 +1,12 @@
 package pages;
 
-public class BasePage {
-// IntelliJ API Decompiler stub source generated from a class file
-    // Implementation of methods is not available
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
-package pages;
-
-    public class BasePage extends net.serenitybdd.core.pages.PageObject {
-        protected BasePage(org.openqa.selenium.WebDriver driver) { /* compiled code */ }
+public class BasePage extends PageObject {
+    protected BasePage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
     }
 }
