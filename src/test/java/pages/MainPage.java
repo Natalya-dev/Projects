1bb8c50e1b5bfeab34867aa1f49bc5d61b8cd07e
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-18 19:31:20 +0300

    Update MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -4,48 +4,47 @@ import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @DefaultUrl("http://automationpractice.com/index.php")
 public class MainPage<Webelement> extends BasePage {
-    @FindBy(css = "a.login")
+    
+@FindBy(css = "a.login")
+private WebElement signInButton;
+    
+@FindBy (xpath = ("//*[@id='create-account_form']/div/p"))
+private WebElement createAnAccountText;
+    
+@FindBy (id = "email")
+private Webelement emailAddressField;
+    
+@FindBy (id = "passwd")
+private WebElement passwordField;
 
-    private WebElement signInButton;
+public MainPage(WebDriver driver) { super(driver); }
 
-    @FindBy (xpath = ("//*[@id='create-account_form']/div/p"))
-            private WebElement createAnAccountText;
-    @FindBy (id = "email")
-            private Webelement emailAddressField;
-    @FindBy (id = "passwd")
-            private WebElement passwordField;
+public void openMainPage() { open(); }
 
-    public MainPage(WebDriver driver) { super(driver); }
+public void clickOnSignInButton() {element(signInButton).click();
 
-    public void openMainPage() { open(); }
-
-
-    public void clickOnSignInButton() {element(signInButton).click();
-
-        public void checkText() {
-            String text = element(signInButton).getText();
-            Pattern pattern = Pattern.compile("(^Please\\senter\\s)");
-            Matcher matcher = pattern.matcher(text);
-
-            if(matcher.find()) {
-                System.out.println("Нашел");
+public void checkText() {
+String text = element(signInButton).getText();
+Pattern pattern = Pattern.compile("(^Please\\senter\\s)");
+Matcher matcher = pattern.matcher(text);
+    if(matcher.find()) {
+    System.out.println("Нашел");
             } else {
-                System.out.println("Нe нашел");
+    System.out.println("Нe нашел");
             }
-
         }
-        public void fillInEmailAddressField (String email) {
-            element (emailAddressField).sendKeys(email);
+                                   
+ public void fillInEmailAddressField (String email) {
+ element (emailAddressField).sendKeys(email);
         }
-
-        public void fillInPasswordField (String password) {
-            element (passwordField).sendKeys(password);
+                                   
+ public void fillInPasswordField (String password) {
+ element (passwordField).sendKeys(password);
         }
     }
 }
