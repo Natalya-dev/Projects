040972c80088fb4663fc3d45ed76e7f3e50bb031
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-21 22:55:57 +0300

    Update FiltersTest.java

--- src/test/java/tests/FiltersTest.java
+++ src/test/java/tests/FiltersTest.java
@@ -3,17 +3,16 @@ package tests;
 import Helper.PropertiesHelper;
 import org.junit.Test;
 
-public class FiltersTest extends SampleTest {
+public class FiltersTest extends SampleTest{
 
     @Test
     public void filtersTest () {
- user.mainPage.openMainPage();
- user.mainPage.clickOnSignInButton();
- user.mainPage.fillEmailAddressField(PropertiesHelper.INSTANCE.getProperties("email"));
- user.mainPage.fillPasswordField(PropertiesHelper.INSTANCE.getProperties("password"));
- user.mainPage.clickOnSubmitLoginButton();
- user.mainPage.clickOnWomenTab();
- user.mainPage.checkOrangeColorsOnItems();
+        user.mainPage.openMainPage();
+        user.mainPage.clickOnSignInButton();
+        user.mainPage.fillEmailAddressField(PropertiesHelper.INSTANCE.getProperties("email"));
+        user.mainPage.fillPasswordField(PropertiesHelper.INSTANCE.getProperties("password"));
+        user.mainPage.clickOnSubmitLoginButton();
+        user.mainPage.clickOnWomenTab();
+        user.mainPage.checkOrangeColorsOnItems();
     }
-
 }
