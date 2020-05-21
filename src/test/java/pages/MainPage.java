0cb255a561c422ca9e2d89b8de42464b784fd944
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-21 23:03:30 +0300

    Update MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,17 +1,20 @@
 package pages;
 
 import net.thucydides.core.annotations.DefaultUrl;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-   @DefaultUrl("http://automationpractice.com/index.php")
+@DefaultUrl("http://automationpractice.com/index.php")
 
-   public class MainPage extends BasePage{
-  
-    @FindBy(css = "a.login")  // Указываем веб элемент и его название:
+public class MainPage extends BasePage{
+
+    @FindBy(css = "a.login")
     private WebElement signInButton;
 
     @FindBy(id = "email")
@@ -20,9 +23,21 @@ import java.util.regex.Pattern;
     @FindBy(id = "passwd")
     private WebElement passwordField;
 
+    @FindBy (id="SubmitLogin")  //////new//////
+    private WebElement getSignInButtonOnLoginPage;
+
+    @FindBy (css ="a[title='Women']")  /////new
+    private WebElement womenTab;
+
+    @FindBy (css = "a[style=' style='background:#F39C11;']") ////new
+    private WebElement orangeBox;
+
+    @FindBy (css ="ul[class='color_to_pick_list clearfix']") //////new
+    private List<WebElement> colorBoxes;
+
     @FindBy(xpath = "//*[@id='create-account_form']/div/p")
     private WebElement createAccountText;
-   
+
     public MainPage(WebDriver driver) {
         super(driver);
     }
@@ -30,24 +45,43 @@ import java.util.regex.Pattern;
     public void openMainPage() {
         open();
     }
- 
+
     public void clickOnSignInButton() {
-    element(signInButton).click();
+        element(signInButton).click();
     }
-   
+
     public void fillEmailAddressField(String email){
-    element(emailField).sendKeys(email);
+        element(emailField).sendKeys(email);
     }
-   
+ 
     public void fillPasswordField(String password){
-    element(passwordField).sendKeys(password);
+        element(passwordField).sendKeys(password);
+    }
+
+    public void clickOnSubmitLoginButton () {  //////////new
+        element(getSignInButtonOnLoginPage).click();
+    }
+    public void clickOnWomenTab() {   //////new
+        element(womenTab).click();
+    }
+    private List<WebElement> getColorBoxes () {   /////new
+        return colorBoxes;
+    }
+
+    public void checkOrangeColorsOnItems () {   /////new
+        int counter = 0;
+        for (int i =0; 1 < getColorBoxes().size();i++) {
+            if (getColorBoxes().get(i).findElements(By.cssSelector("a[style='background:#F39C11;']")).size() == 1) {
+                counter ++;
+            }
+        }
+        Assert.assertEquals(3, counter);
     }
 
     public void checkText(){
-
-     String text = element(createAccountText).getText();
-     Pattern pattern = Pattern.compile("(^Please\\senter\\s)");
-     Matcher matcher = pattern.matcher(text);
+        String text = element(createAccountText).getText();
+        Pattern pattern = Pattern.compile("(^Please\\senter\\s)");
+        Matcher matcher = pattern.matcher(text);
         if(matcher.find()){
             System.out.println("FOUND");
         }else {
