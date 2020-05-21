ec9de8901f38f42f5a87439f46a8c2594e525fcc
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-21 17:45:30 +0300

    Update MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -4,48 +4,48 @@ import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@DefaultUrl("http://automationpractice.com/index.php")
-public class MainPage<Webelement> extends BasePage {
-    @FindBy(css = "a.login")
+   @DefaultUrl("http://automationpractice.com/index.php")
 
+   public class MainPage<Webelement> extends BasePage {
+    
+    @FindBy(css = "a.login")
     private WebElement signInButton;
 
     @FindBy (xpath = ("//*[@id='create-account_form']/div/p"))
-            private WebElement createAnAccountText;
+    private WebElement createAnAccountText;
+    
     @FindBy (id = "email")
-            private Webelement emailAddressField;
+    private Webelement emailAddressField;
+    
     @FindBy (id = "passwd")
-            private WebElement passwordField;
+    private WebElement passwordField;
 
     public MainPage(WebDriver driver) { super(driver); }
 
     public void openMainPage() { open(); }
-
-
+    
     public void clickOnSignInButton() {element(signInButton).click();
 
-        public void checkText() {
-            String text = element(signInButton).getText();
-            Pattern pattern = Pattern.compile("(^Please\\senter\\s)");
-            Matcher matcher = pattern.matcher(text);
-
+    public void checkText() {
+    String text = element(signInButton).getText();
+    Pattern pattern = Pattern.compile("(^Please\\senter\\s)");
+    Matcher matcher = pattern.matcher(text);
             if(matcher.find()) {
                 System.out.println("Нашел");
             } else {
                 System.out.println("Нe нашел");
             }
-
         }
-        public void fillInEmailAddressField (String email) {
-            element (emailAddressField).sendKeys(email);
+       
+    public void fillInEmailAddressField (String email) {
+    element (emailAddressField).sendKeys(email);
         }
 
-        public void fillInPasswordField (String password) {
-            element (passwordField).sendKeys(password);
+     public void fillInPasswordField (String password) {
+     element (passwordField).sendKeys(password);
         }
     }
 }
