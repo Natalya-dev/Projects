c266122f982081c7dc3d8c5e009015edc855e865
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-05-21 17:54:32 +0300

    Update User.java

--- src/test/java/tests/User.java
+++ src/test/java/tests/User.java
@@ -1,17 +1,11 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package tests;
 
 import net.thucydides.core.annotations.Steps;
 import steps.MainSteps;
 
 public class User {
+
     @Steps
     public MainSteps mainPage;
-
-    public User() {
-    }
 }
+
