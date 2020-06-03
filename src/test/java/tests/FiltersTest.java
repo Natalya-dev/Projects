3155cf705d9dfc7cc54b0e522226ed52524b0c58
Natalya-dev <pashkevich_natalya111@mail.ru>
2020-06-03 14:46:52 +0300

    project

---
+++ src/test/java/tests/FiltersTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import Helper.PropertiesHelper;
+import org.junit.Test;
+
+public class FiltersTest extends SampleTest{
+
+    @Test
+    public void filtersTest () {
+        user.mainPage.openMainPage();
+        user.mainPage.clickOnSignInButton();
+        user.mainPage.fillEmailAddressField(PropertiesHelper.INSTANCE.getProperties("email"));
+        user.mainPage.fillPasswordField(PropertiesHelper.INSTANCE.getProperties("password"));
+        user.mainPage.clickOnSubmitLoginButton();
+        user.mainPage.clickOnWomenTab();
+        user.mainPage.checkOrangeColorsOnItems();
+
+    }
+
+}
