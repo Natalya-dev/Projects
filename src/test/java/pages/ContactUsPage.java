6599ec4f63629f1b18678b723b96cf955f801956
Natalya-dev <64799761+Natalya-dev@users.noreply.github.com>
2020-06-03 14:54:41 +0300

    Update ContactUsPage.java

--- src/test/java/pages/ContactUsPage.java
+++ src/test/java/pages/ContactUsPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.FindBy;
 
  public class ContactUsPage extends BasePage {
 
-    @FindBy(id = "uniform-id_contact") //xpath = "//select=[@class='form-control']"
+    @FindBy(id = "uniform-id_contact") 
     private WebElement subjectHeadingDropDownButton;
 
     @FindBy (id = "id_contact")
@@ -30,18 +30,16 @@ import org.openqa.selenium.support.FindBy;
     @FindBy (id = "message")
     private WebElement messageField;
 
-    @FindBy (css = "a[title='Contact Us']")  //??
+    @FindBy (css = "a[title='Contact Us']") 
     private WebElement contactUsButton;
 
     protected ContactUsPage(WebDriver driver) {
         super(driver);
     }
 
-
     public void clickOnSubjectHeadingDropDownButton(String subjectValue) {
         element(subjectHeadingDropDownButton).click();
         selectFromDropdown(subject, subjectValue); // для dropdown
-
     }
 
     public void  fillInEmailAddressField (String email) {
@@ -65,13 +63,10 @@ import org.openqa.selenium.support.FindBy;
    public void clickOnMessageField (String message) {
         element(messageField).sendKeys(message);
 
-}
+   }
 
     public void clickOnContactUsButton() {
         element (contactUsButton).click();
     }
 
     }
-
-
-
